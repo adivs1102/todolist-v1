6f5186e76e6434a1a0b39c66c8d03104b4a01987
adivs1102 <101512203+adivs1102@users.noreply.github.com>
2022-12-22 22:35:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -110,6 +110,7 @@ app.post("/delete",function(req,res){
 app.get("/about",function(req,res){
     res.render("about");
 })
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+const port=process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server started on port ${port}");
 })
